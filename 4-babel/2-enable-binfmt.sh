c3103d1b72d7ce670b0bfb3b75e7c5be513b9808
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2022-03-22 18:57:15 +0100

    ♻️ More reorg

--- 4-babel/2-enable-binfmt.sh
+++ 4-babel/2-enable-binfmt.sh
@@ -1,3 +1,6 @@
-docker run --privileged --rm \
-       multiarch/qemu-user-static --reset --persistent=yes
+#docker run --privileged --rm \
+#       multiarch/qemu-user-static --reset --persistent=yes
+
+docker run --rm --privileged aptman/qus -s -- -p
+
 
