1bb8c567214e8e2e634b0b184390f3f71a0433d1
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2022-10-08 13:30:55 +0200

    ♻️  Minor updates

--- 0-prepare/2-start-arm-instance.sh
+++ 0-prepare/2-start-arm-instance.sh
@@ -1,1 +1,10 @@
-aws ec2 run-instances --key-name=SHA256Ii-jp --instance-type=t4g.medium --image-id=ami-0a0929d6a878fe378
+#!/bin/sh
+# Check https://cloud-images.ubuntu.com/locator/ for image ids
+
+# 20.04
+#AMI=ami-0a0929d6a878fe378
+
+# 22.04
+AMI=ami-0c0f5f4411004770e
+
+aws ec2 run-instances --key-name=SHA256Ii-jp --instance-type=t4g.medium --image-id=$AMI
---
+++ 4-babel/0-disable-binfmt.sh
@@ -0,0 +1,2 @@
+docker run --rm --privileged aptman/qus -- -r
+
