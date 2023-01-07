48fee573c37d7b260ebbc209d84945f995af8526
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2023-01-07 15:17:19 +0100

    🔧 Tweak multiarch example

--- 4-babel/0-disable-binfmt.sh
+++ 4-babel/0-disable-binfmt.sh
@@ -1,2 +1,1 @@
 docker run --rm --privileged aptman/qus -- -r
-
--- 4-babel/2-enable-binfmt.sh
+++ 4-babel/2-enable-binfmt.sh
@@ -1,6 +1,1 @@
-#docker run --privileged --rm \
-#       multiarch/qemu-user-static --reset --persistent=yes
-
 docker run --rm --privileged aptman/qus -s -- -p
-
-
